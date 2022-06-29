cd9c8bc3c734b5643c12c35529228f45d2c7b366
Adeite <olagbenroadeite@gmail.com>
2022-06-29 08:55:36 -0700

    all

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Olagbenro Adeite
+
+"""Print numbers 0 to 98 in decimal and hexadecimal."""
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
