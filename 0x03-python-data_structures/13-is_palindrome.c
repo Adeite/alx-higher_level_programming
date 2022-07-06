d7f699f394047a16156a1fffb0d2cf3f609c520d
Adeite <olagbenroadeite@gmail.com>
2022-07-06 12:58:49 -0700

    nex

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 
-//reverse - reverses the second half of the list
-
+/**
+ * reverse - reverses the second half of the list
+ *
+ * @h_r: head of the second half
+ * Return: no return
+ */
 void reverse(listint_t **h_r)
 {
 	listint_t *prv;
